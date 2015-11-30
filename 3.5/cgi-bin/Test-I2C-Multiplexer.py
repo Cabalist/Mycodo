7d396e8a0b6a82077a2787b84410b14152e1cf92
Ryan Jarvis <cabalist@gmail.com>
2015-11-29 23:06:27 -0800

    remove globals.  Implement argparse

--- 3.5/cgi-bin/Test-I2C-Multiplexer.py
+++ 3.5/cgi-bin/Test-I2C-Multiplexer.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python
+# coding=utf-8
 
 # Select channel of TCA9548A I2C multiplexer
 # Example: sudo ./multiplexer_channel.py 1
 
+import time
+import argparse
+
 import RPi.GPIO as GPIO
 import smbus
-import sys
-import time
-
-I2C_address = 0x70
-if GPIO.RPI_REVISION == 2 or GPIO.RPI_REVISION == 3:
-    I2C_bus_number = 1
-else:
-    I2C_bus_number = 0
 
 def I2C_setup(i2c_channel_setup):
-	bus = smbus.SMBus(I2C_bus_number)
-	bus.write_byte(I2C_address,i2c_channel_setup)
-	time.sleep(0.1)
-	print "TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address))
+    I2C_address = 0x70
+    if GPIO.RPI_REVISION in [2, 3]:
+        I2C_bus_number = 1
+    else:
+        I2C_bus_number = 0
 
-I2C_setup(int(sys.argv[1]))
+    bus = smbus.SMBus(I2C_bus_number)
+    bus.write_byte(I2C_address, i2c_channel_setup)
+    time.sleep(0.1)
+    print("TCA9548A I2C channel status:{}".format(bin(bus.read_byte(I2C_address))))
+
+
+def menu():
+    parser = argparse.ArgumentParser(description='Select channel of TCA9548A I2C multiplexer')
+    parser.add_argument('channel', type=int)
+
+    args = parser.parse_args()
+
+    I2C_setup(args.channel)
+
+if __name__ == "__main__":
+    menu()
