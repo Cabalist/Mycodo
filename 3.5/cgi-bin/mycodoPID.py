c0ae028a63c88dfa02fe8e6af562f3f8dbec0c52
Ryan Jarvis <cabalist@gmail.com>
2015-11-29 22:46:20 -0800

    Whitespace, New style object, init

--- 3.5/cgi-bin/mycodoPID.py
+++ 3.5/cgi-bin/mycodoPID.py
@@ -27,31 +27,36 @@
 #
 # <http://code.activestate.com/recipes/577231-discrete-pid-controller/>
 
-class PID:
+
+class PID(object):
     def __init__(self, P=2.0, I=0.0, D=1.0, Measure_interval=None, Derivator=0, Integrator=0,
-            Integrator_max=500, Integrator_min=-500):
-        self.Kp=P
-        self.Ki=I
-        self.Kd=D
-        self.Measure_interval=Measure_interval
-        self.Derivator=Derivator
-        self.Integrator=Integrator
-        self.Integrator_max=Integrator_max
-        self.Integrator_min=Integrator_min
-        self.set_point=0.0
-        self.error=0.0
+                 Integrator_max=500, Integrator_min=-500):
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+        self.Measure_interval = Measure_interval
+        self.Derivator = Derivator
+        self.Integrator = Integrator
+        self.Integrator_max = Integrator_max
+        self.Integrator_min = Integrator_min
+        self.set_point = 0.0
+        self.error = 0.0
+        self.P_value = None
+        self.I_value = None
+        self.D_value = None
+
     def update(self, current_value):
         """Calculate PID output value from reference input and feedback"""
         self.error = self.set_point - current_value
         self.P_value = self.Kp * self.error
         self.D_value = self.Kd * (self.error - self.Derivator)
         self.Derivator = self.error
-        self.Integrator = self.Integrator + self.error
+        self.Integrator += self.error
         # if self.Integrator > self.Integrator_max:
         #     self.Integrator = self.Integrator_max
         # elif self.Integrator < self.Integrator_min:
         #     self.Integrator = self.Integrator_min
-        if self.Measure_interval is not None: #  New method for regulating Integrator
+        if self.Measure_interval is not None:  # New method for regulating Integrator
             if self.Integrator * self.Ki > self.Measure_interval:
                 self.Integrator = self.Measure_interval / self.Ki
             elif self.Integrator * self.Ki < -self.Measure_interval:
@@ -59,26 +64,36 @@ class PID:
         self.I_value = self.Integrator * self.Ki
         PID = self.P_value + self.I_value + self.D_value
         return PID
+
     def setPoint(self, set_point):
         """Initilize the setpoint of PID"""
         self.set_point = set_point
-        self.Integrator=0
-        self.Derivator=0
+        self.Integrator = 0
+        self.Derivator = 0
+
     def setIntegrator(self, Integrator):
         self.Integrator = Integrator
+
     def setDerivator(self, Derivator):
         self.Derivator = Derivator
-    def setKp(self,P):
-        self.Kp=P
-    def setKi(self,I):
-        self.Ki=I
-    def setKd(self,D):
-        self.Kd=D
+
+    def setKp(self, P):
+        self.Kp = P
+
+    def setKi(self, I):
+        self.Ki = I
+
+    def setKd(self, D):
+        self.Kd = D
+
     def getPoint(self):
         return self.set_point
+
     def getError(self):
         return self.error
+
     def getIntegrator(self):
         return self.Integrator
+
     def getDerivator(self):
         return self.Derivator
